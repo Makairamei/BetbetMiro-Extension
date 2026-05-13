e57d90377854f455af561fb1adbf416afb15a46f
sad25kag <sad25kag@gmail.com>
2026-05-13 10:27:08 +0700

    Test

---
+++ YunshanID/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+cloudstream {
+    mainClassName = "com.betbet.yunshanid.YunshanIDPlugin"
+    language = "id"
+    description = "Donghua & Anime provider YunshanID"
+    authors = listOf("Betbet")
+}
+
+android {
+    namespace = "com.betbet.yunshanid"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
