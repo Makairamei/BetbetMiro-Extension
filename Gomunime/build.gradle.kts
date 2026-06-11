df38263fda75edec61f4df5d8e6fbd4c9a8b5c04
sad25kag <sad25kag@gmail.com>
2026-06-11 18:49:53 +0700

    .

--- Gomunime/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 17
-
-cloudstream {
-    language = "id"
-    description = "Gomunime anime sub Indo dengan resolver loadLinks aktif untuk server B-TUBE, CEPAT/XTWAP, GDRIVE/GDPlayer, Blogger, dan direct media."
-    authors = listOf("Betbet")
-    status = 0
-    tvTypes = listOf("Anime", "AnimeMovie", "OVA")
-}
