cc0265cbea48af80340e7c0a0d79c6890968690b
sad25kag <sad25kag@gmail.com>
2026-06-27 08:35:56 +0700

    fix(doronime): update provider manifest

--- Doronime/build.gradle.kts
+++ Doronime/build.gradle.kts
@@ -1,19 +1,10 @@
-version = 3
+version = 4
 
 cloudstream {
     description = "Doronime provider untuk anime subtitle Indonesia dengan dynamic server/player resolver."
     language = "id"
     authors = listOf("sad25kag")
-
-    /**
-     * Status int:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     */
     status = 1
-
     tvTypes = listOf("Anime", "AnimeMovie", "OVA")
-    iconUrl = "https://www.google.com/s2/favicons?domain=doronime.id&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=doronime.id&sz=64"
 }
