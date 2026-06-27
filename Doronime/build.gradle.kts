d6c66d54e31e632a1fb39f406b8e209cd8a85414
sad25kag <sad25kag@gmail.com>
2026-06-27 13:32:54 +0700

    delete

--- Doronime/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 9
-
-cloudstream {
-    description = "Doronime"
-    language = "id"
-    authors = listOf("sad25kag")
-    status = 1
-    tvTypes = listOf("Anime")
-}
--- Doronime/src/main/kotlin/com/sad25kag/doronime/DoronimeSearchResponseCompat.kt
+++
@@ -1,6 +0,0 @@
-package com.sad25kag.doronime
-
-import com.lagradost.cloudstream3.SearchResponse
-
-internal val SearchResponse.title: String
-    get() = name
